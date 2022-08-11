ecbf5a26994f7f3cb27f69498cdc992ca4fea97f
Rainer Regan <rainercoc@gmail.com>
2022-08-11 19:44:13 +0700

    Creating Seeder for Posts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        if ($this->command->confirm("Do you want to refresh the database?")){
+            $this->command->call('migrate:refresh');
+            $this->command->info('Database was refreshed');
+        }
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            PostSeeder::class
+        ]);
     }
 }
